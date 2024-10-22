f0fb96405a75d99f5158796ff7987189c442e762
Winlypse <nts3@o2.pl>
2024-10-22 14:53:02 +0200

    Initial commit

---
+++ src/main/kotlin/winlyps/immediateRespawn/ImmediateRespawn.kt
@@ -0,0 +1,14 @@
+package winlyps.immediateRespawn
+
+import org.bukkit.plugin.java.JavaPlugin
+
+class ImmediateRespawn : JavaPlugin() {
+
+    override fun onEnable() {
+        // Plugin startup logic
+    }
+
+    override fun onDisable() {
+        // Plugin shutdown logic
+    }
+}
